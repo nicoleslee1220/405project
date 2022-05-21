e7cc509363b6f608c3505ef1c2a0cc72cd364d3c
nicole_lee <nicoles.lee1220@gmail.com>
2022-05-21 10:32:54 -0700

    adding connectDB.R file

---
+++ connectDB.R
@@ -0,0 +1,24 @@
+library(RMySQL)
+
+
+drv <- dbDriver("MySQL")
+
+######################################## simple copy over table
+######################################## simple copy over table
+######################################## simple copy over table
+
+
+xdbsock <- ""
+
+
+xdbuser <- Sys.getenv("MAS405_AWS_PROJ_DB_ROUSER_USER")
+xpw     <- Sys.getenv("MAS405_AWS_PROJ_DB_ROUSER_PW")
+xdbname <- Sys.getenv("MAS405_AWS_PROJ_DB_ROUSER_DBNAME")
+xdbhost <- Sys.getenv("MAS405_AWS_PROJ_DB_ROUSER_HOST")
+xdbport <- as.integer( Sys.getenv("MAS405_AWS_PROJ_DB_ROUSER_PORT") )
+
+
+con <- dbConnect(drv, user=xdbuser, password=xpw, dbname=xdbname, host=xdbhost, port=xdbport, unix.sock=xdbsock)
+
+dbGetInfo(con)
+dbListTables(con)
