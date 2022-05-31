731d018886efc6342dd2408a46a68284c682ab33
Sammy Ou <106421798+sammyou@users.noreply.github.com>
2022-05-31 15:20:11 -0700

    fix id back to full range

--- downloadJson.R
+++ downloadJson.R
@@ -6,7 +6,7 @@ library(SSsimple)
 library(rjson)
 
 
-## Setting directory for .json downlad. xpath_scrape is where .json will download into
+## Setting directory for .json download. xpath_scrape is where .json will download into
 xpath_main_data <- Sys.getenv("PATH_MY_MAIN_DATA")
 
 xpath_scrape <-
@@ -27,9 +27,10 @@ xAPIkey
 
 
 
-## Get Recipe Information: Spoonacular has about 1.7 million recipes, downloading a subset
-ii <- 5600 
-for(ii in 5600:6000) {
+## Get Recipe Information: Spoonacular has about 1.15 million recipes, downloading a subset
+# ii_vec <- sample(1:1500000, 7000, replace = FALSE)
+ii_vec <- 5600:13000 # assuming recipe ids are not correlated; ~7400 recipes
+for(ii in ii_vec) {
 
     xthis_url <-
       paste0(
@@ -46,18 +47,16 @@ for(ii in 5600:6000) {
       cat("Could not read recipe", ii, "\n")
     }else{
       xtn_out <- paste0("recipe_", ii, "_spoonacular.json")
-      writeLines( xthisJSON, file.path(xpath_scrape, xtn_out) )
+      writeLines( xthisJSON, file.path(xpath_scrape, xtn_out) ) # overwrites file if same id exists
       cat("Done getting recipe", ii, "\n" )
     }
                      
-    Sys.sleep(1)
+    Sys.sleep(sample(c(1,3,5), 1))
     
 }
 
-
-## Taste: Spoonacular has about 1.7 million recipes, downloading a subset
-ii <- 12800 
-for(ii in 12800:13000) {
+## Taste: Spoonacular has about 1.15 million recipes, downloading a subset
+for(ii in ii_vec) {
   
   xthis_url <-
     paste0(
@@ -78,9 +77,37 @@ for(ii in 12800:13000) {
     cat("Done getting recipe", ii, "\n" )
   }
   
-  Sys.sleep(1)
+  Sys.sleep(sample(c(1,3,5), 1))
   
 }
 
+## Instructions
+for(ii in ii_vec) {
+  
+  xthis_url <-
+    paste0(
+      "https://api.spoonacular.com/recipes/",
+      ii,
+      "/analyzedInstructions",
+      "?apiKey=",
+      xAPIkey
+    )
+  
+  xthisJSON <- try(suppressWarnings(readLines(con=xthis_url)))
+  
+  if("try-error" %in% class(xthisJSON)){
+    cat("Could not read recipe", ii, "\n")
+  }else{
+    xtn_out <- paste0("instructions", ii, "_spoonacular.json")
+    writeLines( xthisJSON, file.path(xpath_scrape, xtn_out) )
+    cat("Done getting recipe", ii, "\n" )
+  }
+  
+  Sys.sleep(sample(c(1,2,3), 1))
+  
+}
+
+# fl <- list.files(xpath_scrape, pattern = "^instructions")
+# file.rename(fl, sub("(.{12})(.*)", "\\1_\\2", fl))
 
 
