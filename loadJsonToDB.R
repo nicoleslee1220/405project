2bbf51dba45a19019be5e8112c27db5f9707a41e
Sammy Ou <106421798+sammyou@users.noreply.github.com>
2022-05-31 15:21:00 -0700

    changed file path for new json folders

--- loadJsonToDB.R
+++ loadJsonToDB.R
@@ -21,7 +21,6 @@ dbListTables(con)
 
 ##Get Recipe Information: creating table in DB
 xpath_main_data <- Sys.getenv("PATH_MY_MAIN_DATA")
-xpath_scrape <- file.path(xpath_main_data, "spoonacular")
 xtableName <- "spoonacularRecipe"
 xbool.tableExists <- dbExistsTable(con, xtableName) ; xbool.tableExists
 
@@ -55,14 +54,9 @@ if(!xbool.tableExists) {
   xx <- dbGetQuery(con, qstr)
 }
 
-
-
-
-
-
 ##Get Recipe Information: Inserting json data into DB
 
-xgfn <- list.files(xpath_scrape, pattern="^recipe")
+xgfn <- list.files(file.path(xpath_main_data, "spoonacular_recipe"), pattern="^recipe")
 xgfn_sub <- xgfn
 
 ii <- 1
@@ -138,60 +132,9 @@ for(ii in 1:length(xgfn_sub)) {
 }
 
 
-##Get Recipe Information: Adding readyInMinutes column from json into DB
-
-xpath_main_data <- Sys.getenv("PATH_MY_MAIN_DATA")
-xpath_scrape <- file.path(xpath_main_data, "/spoonacular_Recipe")
-xtableName <- "spoonacularRecipe"
-
-####adding column 
-qstr <- paste0("ALTER TABLE ", xtableName , " ADD readyInMinutes INT(7)")
-xx <- dbGetQuery(con, qstr)
-
-xgfn <- list.files(xpath_scrape, pattern="^recipe")
-xgfn_sub <- xgfn
-
-ii <- 1
-for(ii in 1:length(xgfn_sub)) {
-  
-  xthis_fn <- xgfn_sub[ii] 
-  xxx <- strsplit(xthis_fn, "_")[[1]]
-  DT <- xxx[2] #DT is the recipe number
-  recipeID <- xxx[2]
-  
-  
-  xthis_ls <- fromJSON( file=file.path(xpath_scrape, xthis_fn) )
-  
-  
-  xthis_ls[[c("readyInMinutes")]]
-  
-  
-  
-  qstr <- paste0(
-    "INSERT INTO ", xtableName, " (DT, readyInMinutes) ",
-    # "INSERT INTO ", xtableName, " (DT, recipeID, vegetarian, pricePerServing) ",
-    
-    " VALUES ",
-    "('",
-    DT, "', '",
-    as.integer(readyInMinutes), "')"
-  )
-  
-  xx <- try(dbGetQuery(con, qstr), silent=TRUE)
-  
-  if( "try-error" %in% class(xx) ) {
-    cat("SQL insert into Team Table failed for recipe# ", DT, "\n")
-  } else {
-    cat("Successfully inserted recipe# ", DT, "into Table", "\n")
-  }
-  
-}
-
 
 
 ##Taste: creating table in DB
-xpath_main_data <- Sys.getenv("PATH_MY_MAIN_DATA")
-xpath_scrape <- file.path(xpath_main_data, "spoonacular")
 xtableName <- "spoonacularTaste"
 xbool.tableExists <- dbExistsTable(con, xtableName) ; xbool.tableExists
 
@@ -221,7 +164,7 @@ if(!xbool.tableExists) {
 
 ##Taste: Inserting json data into DB
 
-xgfn <- list.files(xpath_scrape, pattern="^taste")
+xgfn <- list.files(file.path(xpath_main_data, "spoonacular_taste"), pattern="^taste")
 xgfn_sub <- xgfn
 
 ii <- 1
@@ -270,4 +213,52 @@ for(ii in 1:length(xgfn_sub)) {
   }
   
 }
+
+## Instructions 
+# create table 
+qstr <- paste("CREATE TABLE spoonacularInstruct",
+              "(DT VARCHAR (10) NOT NULL,", 
+              "instructions VARCHAR(65535),", 
+              "PRIMARY KEY (DT))")
+instruct_bool <- dbExistsTable(con, "spoonacularInstruct")
+if (!instruct_bool){
+  dbGetQuery(con, qstr)
+}
+dbListTables(con)
+
+instruct_fl <- list.files(file.path(xpath_main_data, "spoonacular_instructions"), pattern = "^instructions")
+  # note: id 10000 comes before 5000
+  # file.size(file.path(xpath_scrape, fileName))
+    # some files contain nothing; need to be filtered out during data wrangling
+for (ii in 1:length(instruct_fl)){
+  fileName <- instruct_fl[ii]
+  id <- strsplit(fileName, "_")[[1]][[2]]
+  rList <- fromJSON(file = file.path(xpath_scrape, fileName))
+  allSteps <- ""
+  if (length(rList) != 0){
+    for (jj in 1:length(rList)){
+      stepList <- rList[[jj]]$steps
+      # gather steps 
+      if (length(stepList) != 0){
+        for (kk in 1:length(stepList)){
+          newStep <- stepList[[kk]]$step
+          allSteps <- paste(allSteps, newStep)
+        }
+      }
+    }
+  }
+  qstr <- paste0("INSERT INTO spoonacularInstruct ", 
+                 "(DT, instructions) VALUES (", 
+                 "'", id, "', ", 
+                 "'", allSteps, "')")
+  cmd <- try(dbGetQuery(con, qstr), silent = TRUE)
+  # error handling
+  if("try-error" %in% class(cmd)){
+    cat("ID", id, "failed to insert successfully. \n")
+  } else {
+    cat("Successfully inserted ID", id, "in database. \n")
+  }
+}
+
+
 
